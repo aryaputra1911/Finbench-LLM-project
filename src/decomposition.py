8bfd3c1a1ebe0ca31ff0bb6866bf307dc115342b
Muhammad Arya Putra Handrian <aryaputraa1911@gmail.com>
2026-01-08 21:47:40 +0700

    Update decomposition.py

--- src/decomposition.py
+++ src/decomposition.py
@@ -52,8 +52,9 @@ def process_all_markdowns(input_root, output_dir):
             print(f"Gagal memproses {md_file.name}: {e}")
 
 if __name__ == "__main__":
-    input_folder = r"\data\processed\markdown"
-    output_folder = r"\data\processed\decomposed"
+    input_folder = r"data\processed\markdown"
+    output_folder = r"data\processed\decomposed"
     
 
     process_all_markdowns(input_folder, output_folder)
+
